82a65e6250b8b1eb3c5290e375d70e039d43b5ac
nsoler <nsoler@irbbarcelona.org>
2020-12-02 13:27:41 +0100

    can use InChi as well

--- singularity/run_BBB_predictor.py
+++ singularity/run_BBB_predictor.py
@@ -6,17 +6,18 @@ usage="""
 Blood-brain barrier (BBB) predictor:
 ------------------------------------
 Classifier that will predict if a chemical coumpound will pass the Blood-brain barrier.
-SBNB lab (IRB Barcelona) - Nov 2020
+SBNB lab (IRB Barcelona) - Nov 2020.
 
-USAGE: BBBpredictor myfile.csv
+USAGE: BBBpredictor myfile.tsv [SMILES or INCHI]
 
-Where my myfile.csv is an input text file that contains two columns separated by commas or spaces/tabs
-COLUMN 1: compound id (ex:1)
-COLUMN 2: compound SMILES string
+Where my myfile.tsv is an input text file that contains two columns separated by TABS.
+COLUMN 1: compound unique id (ex:1 or a molecule name without tab characters inside).
+COLUMN 2: compound SMILES string.
 
-NOTE: Lines starting with '#' will be ignored
+NOTE: The default format is SMILES and can be turned into InChI if the second argument INCHI is provided.
+NOTE: Lines starting with '#' as well as empty lines are ignored.
 
-OUTPUT: csv file containing the prediction for each compound
+OUTPUT: tsv file containing the prediction for each compound
 
 PREDICTION LEGEND: passes the BBB?
 0: no
@@ -40,10 +41,23 @@ from signaturizer import Signaturizer
 
 inputFile=sys.argv[1]
 entries=dict()
-outputDic= dict() 
+outputDic= dict()
 
-path_to_model="/opt/rf_from_signZ_paper_full.joblib"
-#path_to_model="../MLmodels/rf_from_signZ_paper_full.joblib"
+formatMol= "SMILES"
+if len(sys.argv)>2 and sys.argv[2].upper() == 'INCHI':
+    formatMol='INCHI'
+
+print("INPUT FORMAT: {}\n".format(formatMol))
+
+rootModels="/opt/NNmodels"    # in simg
+#rootModels="NNmodels"        # test
+
+path_to_model="/opt/rf_from_signZ_paper_full.joblib"   # in simg
+#path_to_model="../MLmodels/rf_from_signZ_paper_full.joblib"    # test
+
+listModels=[os.path.join(rootModels,x+y) for x in "A B C D E".split() for y in "1 2 3 4 5".split()]
+
+
 RFmodel= joblib.load(path_to_model)
 
 # Parsing the input file
@@ -52,17 +66,15 @@ with open(inputFile) as f:
         lines=f.readlines()
         for l in lines:
             if len(l.strip())>0 and not l.startswith('#'):
-                if ',' in l:
-                    li= l.split(',')
-                else:
-                    li= l.split()
+                li= l.split('\t')
 
                 entries[li[0].strip()] = li[1].strip()
 
     except Exception as e:
         print("ERROR")
         print(e)
-        print("Quitting")
+        print("Quitting, please check your input file!\n")
+        print(usage)
         sys.exit(1)
 
 # Output dict
@@ -70,26 +82,24 @@ outputDic= dict()
 
 # load the predictor for ALL spaces (the global signature)
 print("Initializing the signaturizer, please wait..")
-sign = Signaturizer('GLOBAL')
+sign = Signaturizer(model_name=listModels, local=True)
 
 
 # Submit all molecules to the signaturizer
 smiles = list(entries.values())
 clefs= list(entries.keys())
 
-print("input smiles")
+print("\n INPUT:")
 print(smiles)
 
-
 # Predict the signatures
-print("\nCalculating the signatures for the {:d} input molecules".format(len(entries)))
-signZlist = sign.predict(smiles).signature
+print("\nCalculating the signatures for the {:d} input molecules:\n".format(len(entries)))
+signZlist = sign.predict(smiles, keytype=formatMol).signature
+print("SHAPE of the returned signatures",signZlist.shape,'\n')
 
 assert (len(signZlist) == len(clefs)),"ERROR: The signature list and compound id list don't have the same length!"
 
 
-
-print("LEN",len(clefs))
 # dict compoundid: signZ 
 for i in range(len(clefs)):
     outputDic[clefs[i]] = signZlist[i]
@@ -109,10 +119,11 @@ for i in range(X.shape[0]):
     else:
         y_final.append(y_hat[i])
 
-print("No signature for the following {:d} coumpounds".format(len(problems)))
-print(problems)
+if len(problems)>0:
+    print("WARNING: no signature for the following {:d} coumpounds".format(len(problems)))
+    print(problems)
 
-outFile= os.path.basename(inputFile).split('.')[0]+"_BBBpredictions.csv"
+outFile= os.path.basename(inputFile).split('.')[0]+"_BBBpredictions.tsv"
 with open(outFile,'w') as f:
     f.write("#Brain blood barrier predictions\n")
     f.write("#1: pass, 0: does not pass, -1: error\n")
